2e341bcfbaefffb09f6768c68d25129d30a6d781
Arnaldo Mera <nardy.allstars@gmail.com>
2024-05-12 12:35:15 +0200

    some basic css and app.jsx composition

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,20 @@ import './App.css'
 function App() {
 
   return (
-    <>asdasdasdasdasd
+    <>
+      <section className="NavBar">
+        <h2>Home Page</h2>
+        <h2>Races</h2>
+        <h2>Store</h2>
+        <h2>Contact</h2>
+        <img src="" alt="Game Icon" />
+      </section>
+
+      <section className="Footer">
+        <div>
+          <h3>My Footer</h3>
+        </div>
+      </section>
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,17 @@ import supabase from "../supabase/config"
 
 function HomePage() {
     return (
+        
         <>
-            <h1>Informacion general del juego</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate repudiandae illo atque natus molestiae
-                quam doloremque nam? Quisquam quos dicta perspiciatis, neque, modi ducimus deleniti, dolorum veritatis hic ad
-                numquam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ut atque natus asperiores voluptatibus eaque!
-                Facilis corrupti deserunt similique molestias voluptate incidunt, omnis, explicabo voluptatibus consequuntur dignissimos
-                delectus fuga
-            </p>
+            <section className="Main Info">
+                <h1>Informacion general del juego</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate repudiandae illo atque natus molestiae
+                    quam doloremque nam? Quisquam quos dicta perspiciatis, neque, modi ducimus deleniti, dolorum veritatis hic ad
+                    numquam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ut atque natus asperiores voluptatibus eaque!
+                    Facilis corrupti deserunt similique molestias voluptate incidunt, omnis, explicabo voluptatibus consequuntur dignissimos
+                    delectus fuga
+                </p>
+            </section>
         </>
 
     )
