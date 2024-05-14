15c9d5e0fd38145f84fdf1060449aceadc6a7f8a
Arnaldo Mera <nardy.allstars@gmail.com>
2024-05-14 17:41:35 +0200

    improved users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,21 +3,24 @@ import supabase from "../supabase/config";
 function Users({ users }) {
     return (
         <>
-            {
-                users.map((user) => {
-                    return (
-                        <>
-                            <div className="user-card">
-                                <img className="user-img" src={user.icon} alt="asd" />
-                                <h3 className="user-name">{user.Name}</h3>
-                                <h3 className="user-faction">{user.faction}</h3>
-                                <h3 className="user-class">{user.Class}</h3>
-                                <h3 className="lvl">{user.lvl}</h3>
-                            </div>
-                        </>
-                    )
-                })
-            }
+            <div className="user-page">
+                {
+                    users.map((user) => {
+                        return (
+                            <>
+                                <div className="user-card">
+                                    <img className="user-img" src={user.icon} alt="asd" />
+                                    <h3 className="user-name">{user.Name}</h3>
+                                    <h3 className="user-faction">{user.faction}</h3>
+                                    <h3 className="user-class">{user.Class}</h3>
+                                    <h3 className="user-lvl">{user.lvl}</h3>
+                                    <button className="delete-user">Delete</button>
+                                </div>
+                            </>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
