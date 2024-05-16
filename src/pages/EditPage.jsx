f996070d7983a542b85354de375e79a2cb6cd3fa
Arnaldo Mera <nardy.allstars@gmail.com>
2024-05-16 12:33:41 +0200

    added some things to the edit page

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 function EditPage() {
+
     return (
-        <>asdasd
-        </>
+        <div className="form-content">
+            <form className="formulary">
+            <label className="label-name" htmlFor="Name">Edit Profile</label>
+                <input  placeholder="Edit User Name" type="text" />
+                <select name="faction">
+                        <option value="">-- None --</option>
+                        <option value="Insectoid">Insectoid</option>
+                        <option value="Humanoid">Humanoid</option>
+                    </select>
+                <input  placeholder="Edit User Name" type="text" />
+                <button className="submit" type="submit">Submit</button>
+                <Link to="/Users"><button className="back-back">Go back</button></Link>
+            </form>
+        </div>
     )
 }
 
